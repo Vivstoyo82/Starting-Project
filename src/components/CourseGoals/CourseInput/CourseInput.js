0aa2df7b3f6deaad9ded0ba652f408cddd3f8a0b
Vivstoyo0082 <vivekbokoliya0082@gmail.com>
2022-12-31 20:15:13 +0530

    Dynamic Styling

---
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label>COURSE GOAL</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div>
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+        <Button type="submit">Add Goal</Button>
+      </div>
+    </form>
+  );
+};
+
+export default CourseInput;
